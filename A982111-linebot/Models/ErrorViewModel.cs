78f06f4736aa7b15302e3ab1625fef69caa2a49b
QIAN-RONG <tinalee08060116@gmail.com>
2023-05-09 15:54:27 +0800

    init

---
+++ A982111-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A982111_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
